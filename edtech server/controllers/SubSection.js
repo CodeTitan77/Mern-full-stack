8a44dee6f8079ef76dc47fe4ce60e6cee1db2f31
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2024-11-09 07:37:17 +0530

    updated subsection controller

--- edtech server/controllers/SubSection.js
+++ edtech server/controllers/SubSection.js
@@ -24,5 +24,39 @@ exports.createSubSection= async(req,res)=>{
 
             });
         }
+        const uploadDetails = await uploadImageToCloudinary(video,process.env.FOLDER_NAME);
+        const subSectionDetails= await Subsection.create({
+            title:title,
+            timeDuration:timeDuration,
+            description:description,
+            videoUrl:uploadDetails.secure_url,
+
+        })
+        const updatedSection= await Section.findByIdAndUpdate({_id:sectionId},
+            {
+                $push:{
+                    subSection:subSectionDetails._id,
+                }
+            },
+            {new:true}
+        );
+        return res.status(200).json({
+            success:true,
+            message:'Sub Section created Successfully',
+            updatedSection,
+
+        });
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error",
+            error:error.message,
+
+        })
+
     }
 }
+// update Subsection hw 
+//delete subsection
