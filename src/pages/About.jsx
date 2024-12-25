16c803e499d53125e4eb7154e2729995bebb8470
Himanshu Trivedi <Himanshutrivedi421@gmail.com>
2024-12-25 07:17:40 +0530

    Learning grid

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,8 @@ import BannerImage1 from "../assets/Images/aboutus2.webp"
 import BannerImage2 from "../assets/Images/aboutus3.webp"
 import BannerImage3 from "../assets/Images/aboutus3.webp"
 import Quote from './../components/core/AboutPage/Quote';
-
+import FoundingStory from "../assets/Images/FoundingStory.png"
+import StatsComponent from "../components/core/AboutPage/Stats"
 
 const About = () => {
   return (
@@ -32,6 +33,52 @@ const About = () => {
             </div>
 
         </section>
+        {/* //section3 */}
+
+        <section>
+            <div>
+                {/* founding story */}
+                <div className='flex flex-row'>
+                    <div>
+                        <h1>Our Founding Story</h1>
+                        <p>
+                        Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.
+                        </p>
+                        <p>
+                        As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.  
+                        </p>
+                       
+                    </div>
+                    {/* founding */}
+                    <div>
+                     <img src={FoundingStory}></img>        
+                     </div>
+                </div>
+               {/* vision and mission par */}
+               <div className='flex flex-row'>
+                {/* left */}
+                <div>
+                    <h1>Our Vision</h1>
+                 <p>
+                 With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.
+                 </p>
+                </div>
+                {/* right */}
+                <div>
+                    <h1>
+                    Our Mission
+                    </h1>
+                    <p>
+                    our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.
+                    </p>
+
+                </div>
+               </div>
+            </div>
+
+        </section>
+        {/* section4 */}
+        <StatsComponent/>
       
     </div>
   )
